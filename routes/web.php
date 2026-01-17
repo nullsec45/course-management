ee03fc8cc9e3ef31339e8660786d30987428135d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-18 01:55:04 +0700

    feat : config for login google & facebook.

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,29 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\MembershipController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\VideoController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/report-tracker/{id}', [HomeController::class, 'tracker']);
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/membership', [HomeController::class, 'membership'])->name('membership');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/membership/checkout/{id}', [HomeController::class, 'checkoutPage'])->name('membership.checkout');
+    Route::post('/membership/process', [HomeController::class, 'processCheckout'])->name('membership.process');
+
 
     Route::prefix('dashboard')->name('dashboard.')->group(function () {
 
 
-        // Route::get('/', [Dashbnoa::class, 'dashboard'])->middleware(['role:ADMIN'])->name('index');
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-        Route::middleware('role:ADMIN')->controller(UserController::class)
+        Route::middleware('role:Admin')->controller(UserController::class)
             ->prefix('users')
             ->name('users.')
             ->group(function () {
@@ -34,24 +38,25 @@ Route::middleware('auth')->group(function () {
                 Route::get('/create', 'create')->name('create');
                 Route::post('/store', 'store')->name('store');
                 Route::put('/{id}', 'update')->name('update');
+                Route::delete('/{id}', 'destroy')->name('destroy');
             });
 
 
 
-        Route::middleware('role:ADMIN')->controller(MembershipController::class)
+        Route::middleware('role:Admin')->controller(MembershipController::class)
             ->prefix('memberships')
             ->name('memberships.')
             ->group(function () {
                 Route::get('/', 'index')->name('index');
-                Route::get('/{id}/show', 'show')->name('show');
                 Route::get('/create', 'create')->name('create');
+                Route::get('/{id}/show', 'show')->name('show');
                 Route::post('/store', 'store')->name('store');
                 Route::get('/{id}/edit', 'edit')->name('edit');
                 Route::put('/{id}', 'update')->name('update');
                 Route::delete('/{id}', 'destroy')->name('destroy');
             });
 
-        Route::middleware('role:ADMIN')->controller(CategoryController::class)
+        Route::middleware('role:Admin')->controller(CategoryController::class)
             ->prefix('categories')
             ->name('categories.')
             ->group(function () {
@@ -71,7 +76,7 @@ Route::middleware('auth')->group(function () {
                 Route::get('/', 'index')->name('index');
                 Route::get('/{id}/show', 'show')->name('show');
 
-                Route::middleware('role:USER')->group(function () {
+                Route::middleware('role:Admin')->group(function () {
                     Route::get('/create', 'create')->name('create');
                     Route::post('/store', 'store')->name('store');
                     Route::get('/{id}/edit', 'edit')->name('edit');
@@ -87,7 +92,7 @@ Route::middleware('auth')->group(function () {
                 Route::get('/', 'index')->name('index');
                 Route::get('/{id}/show', 'show')->name('show');
 
-                Route::middleware('role:USER')->group(function () {
+                Route::middleware('role:Admin')->group(function () {
                     Route::get('/create', 'create')->name('create');
                     Route::post('/store', 'store')->name('store');
                     Route::get('/{id}/edit', 'edit')->name('edit');
