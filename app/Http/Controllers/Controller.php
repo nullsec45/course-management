03373ed2bb08866cf6764354e43a19bc5681597c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-18 01:49:22 +0700

    feat : consturct helper in parent controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Helpers;
+
 abstract class Controller
 {
-    //
+
+    protected $helper;
+
+    public function __construct()
+    {
+        $this->helper = new Helpers();
+    }
 }
